2ef8ec6415a18685360ce62ce1f5ab4e6fb9090a
Adel Mahmoud <dolla2924@gmail.com>
2026-01-08 11:14:23 +0200

    add
    content

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const btn_Lessons_ID =document.getElementById("btn_Lessons");
+const btn_homework_ID =document.getElementById("btn_homework");
+const Lessons_ID =document.getElementById("Lessons");
+const homework_ID =document.getElementById("homework");
+
+
+homework_ID.style.display='none';
+btn_Lessons_ID.addEventListener('click',function(){
+    Lessons_ID.style.display='block';
+    homework_ID.style.display='none';
+})
+btn_homework_ID.addEventListener('click',function(){
+    Lessons_ID.style.display='none';
+    homework_ID.style.display='block';
+})
